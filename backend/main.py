21a54404298d042c4787aacaa71bc2aba33c1f5f
benjmnxu <benxu@seas.upenn.edu>
2024-04-20 23:36:10 -0400

    bypass add_middleware

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
 from dotenv import load_dotenv
 import os
 
@@ -8,9 +9,6 @@ from routes import router
 
 load_dotenv()
 
-app = FastAPI()
-
-#app.mount("/logs", StaticFiles(directory="logs"), name="logs")
 origins = [
     "http://localhost",
     "http://localhost:8080",
@@ -18,13 +16,17 @@ origins = [
     "https://cleric-extractor-client.onrender.com/"
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    max_age=86400
-)
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+        max_age=86400
+    )
+]
+
+app = FastAPI(middleware=middleware)
 
 app.include_router(router)
