2771465ce7fde23d21c1c5d12d8eaa6b1245da96
benjmnxu <benxu@seas.upenn.edu>
2024-04-20 23:43:57 -0400

    name change

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:3000",
-    "https://cleric-extractor-client.onrender.com/"
+    "https://cleric-extractor-client.onrender.com"
 ]
 
 middleware = [
