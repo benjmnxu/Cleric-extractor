ba61b11bf457f6a1fbddc0cb9b556e629cbd3f94
benjmnxu <benxu@seas.upenn.edu>
2024-04-20 23:19:37 -0400

    origins change

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:3000",
-    "https://cleric-extractor.vercel.app"
+    "https://cleric-extractor-client.onrender.com/"
 ]
 
 app.add_middleware(
